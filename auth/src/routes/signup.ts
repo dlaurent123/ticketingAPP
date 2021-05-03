3cd6efcc5c880b5793f7cfde0d3a432f9683b031
dannyl <danilovlaurent@pursuit.org>
2021-05-03 15:49:08 -0400

    install expres validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,9 +1,12 @@
 import express from "express"
+import {body} from "express-validator"
 
 const router = express.Router()
 
 
 router.post("/api/users/signup",(req,res) =>{
+    const {email,password} = req.body
+
     try {
         res.send("IT WORKS!")
       } catch (error) {
