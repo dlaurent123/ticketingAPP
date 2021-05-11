cd601dc571c103261df9b20b04f456e223fd3dde
dannyl <danilovlaurent@pursuit.org>
2021-05-11 16:52:59 -0400

    install mongoose db and create config file to create its pod and create user model

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -23,6 +25,21 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`SERVER IS RUNNING ON PORT ${PORT}!`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("connected to mongo ddb");
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`SERVER IS RUNNING ON PORT ${PORT}!`);
+  });
+};
+
+start();
