1ae41eb4240b4182ca88c19d3fc9aa5260922a47
dannyl <danilovlaurent@pursuit.org>
2021-05-13 17:01:11 -0400

    install json web token and its types files

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import mongoose from "mongoose";
-
+import cookieSession from "cookie-session";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -12,7 +12,17 @@ import { NotFoundError } from "./errors/not-found-error";
 
 const PORT = 3000;
 const app = express();
+
+app.set("trust proxy", true);
+
 app.use(json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signupRouter);
