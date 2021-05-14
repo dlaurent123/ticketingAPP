6773e77fe6b7dd5b7a2f27780ed917cad2ed03c0
dannyl <danilovlaurent@pursuit.org>
2021-05-14 00:33:42 -0400

    create signin handler

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import jwt from "jsonwebtoken";
+import { BadRequestError } from "../errors/bad-request-error";
 import { validateRequest } from "../middleware/validate-request";
+import { User } from "../models/users";
+import { Password } from "../tools/password";
 
 const router = express.Router();
 
@@ -14,7 +18,37 @@ router.post(
       .withMessage("You must apply a valid password"),
   ],
   validateRequest,
-  (req: Request, res: Response) => {}
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    const isMatchedPassword = Password.compare(existingUser.password, password);
+
+    if (!isMatchedPassword) {
+      throw new BadRequestError("Invalid credentials");
+    }
+
+    // Generate json web token
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.password,
+      },
+      process.env.JWT_KEY!
+    );
+
+    //  store jwt in cookies session
+
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).send(existingUser);
+  }
 );
 
 export { router as signinRouter };
