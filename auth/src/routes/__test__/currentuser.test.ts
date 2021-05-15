1eee6ba35167d2a3a20ab7f1874ecff519cfcf55
dannyl <danilovlaurent@pursuit.org>
2021-05-14 21:11:22 -0400

    create test suit for current user

---
+++ auth/src/routes/__test__/currentuser.test.ts
@@ -0,0 +1,25 @@
+import { app } from "../../app";
+import request from "supertest";
+
+const email = "test@test.com";
+
+it("responds with detail about the currrent user", async () => {
+  const cookie = await global.signin();
+
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .set("Cookie", cookie)
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser.email).toEqual(email);
+});
+
+it("responds with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
+});
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,12 +1,12 @@
 import express from "express";
-
 import { currentUser } from "../middleware/currentUser";
-import { requireAuth } from "../middleware/require-auth";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", currentUser, requireAuth, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  res.send({
+    currentUser: req.currentUser !== undefined ? req.currentUser : null,
+  });
 });
 
 export { router as currentUserRouter };
