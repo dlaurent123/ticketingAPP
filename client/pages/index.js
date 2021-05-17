1886afb962cc132df3d94e72f00828c38fedf2d9
dannyl <danilovlaurent@pursuit.org>
2021-05-17 17:44:25 -0400

    create server side rendering logic to communicate with pods

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,34 @@
-const App = () => {
+import axios from "axios";
+
+const LandingPage = (data) => {
   return <h1>Landing page</h1>;
 };
 
-export default App;
+// ingress-nginx = namespace
+// ingress-nginx-controller = service name
+// cross name space communitcation url format "http://serviceName.namespace.svc.cluster.local" with options object to specify domain name in headers
+
+LandingPage.getInitialProps = async ({ req }) => {
+  req.headers.Host = "tickets.dev";
+
+  if (typeof window === "undefined") {
+    try {
+      const { data } = await axios.get(
+        "http://35.243.128.119:80/api/users/currentuser",
+        {
+          headers: req.headers,
+        }
+      );
+
+      return data;
+    } catch (error) {
+      console.log("errors", error);
+    }
+  } else {
+    const { data } = await axios.get("/api/users/currentuser");
+
+    return data;
+  }
+};
+
+export default LandingPage;
