36004f26e4c1c2cba189b20ddb4f218ebc7e5efc
dannyl <danilovlaurent@pursuit.org>
2021-05-19 01:59:04 -0400

    create header component

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,9 +5,8 @@ import axios from "axios";
 // cross name space communitcation url format "http://serviceName.namespace.svc.cluster.local" with options object to specify domain name in headers
 
 const buildClient = ({ req }) => {
-  req.headers.Host = "tickets.dev";
-
   if (typeof window === "undefined") {
+    req.headers.Host = "tickets.dev";
     return axios.create({
       baseURL: "http://35.243.128.119:80",
       headers: req.headers,
